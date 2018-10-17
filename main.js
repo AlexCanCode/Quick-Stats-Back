eb13c07ad25eca67abe6fc7adee98f94ce6e027a
Alex Richards <AlexCanCode18@gmail.com>
2018-10-17 09:20:01 -0600

    set scraper to scrape daily at 3am

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-'use strict' //I anticipate this file will do more, such as control the flow of the back-end - if not it becomes redundent and should be removed. RIght now it just takes the formatted stats and exports them in the same format they came in as. 
-const cron = require('cron');
+'use strict' 
+//for server
 const http = require('http');
 const stats = require('./formattedStatsObject.js');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000; //CHANGE BACK TO 8080? 
 
 http.createServer(function(req, res) {
 	res.statusCode = 200;
@@ -12,6 +12,41 @@ http.createServer(function(req, res) {
 }).listen(port, function() {
 	console.log("server started");
 });
+
+//for scraping job
+const fs = require('fs'); //Unused module? 
+const {PythonShell} = require('python-shell');
+const scriptPath = "./scraper/webScraper.py";
+
+function dailyScrape() {
+	const pyshell = new PythonShell(scriptPath);
+	const scrapeDate = new Date();
+
+	pyshell.on('message', function(message) {
+		console.log(message);
+	});
+
+	pyshell.end(function (err) {
+		if(err){
+			throw err;
+		}
+		const finalFormattedStats = require('./format.js'); //need to import the written JSON file from the formatter.
+		const finalStatObjectJSON = JSON.stringify(finalFormattedStats);
+
+		console.log("scraped at " + scrapeDate);
+	})
+}
+
+dailyScrape();
+
+
+
+let CronJob = require('cron').CronJob;
+new CronJob('0 3 * * *', function() {
+	dailyScrape();
+}, null, true, 'America/Denver');
+
+
 
 
 
