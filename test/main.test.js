8b447ab311b6bc3d180cd8435340f20378701950
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-21 11:44:46 -0700

    remove unecessary files

--- test/main.test.js
+++
@@ -1,8 +0,0 @@
-const rewire = require('rewire');
-const assert = require('assert');
-
-const app = rewire('../../Quick Stats/main/main.js')
-
-checkBadValue = app.__get__('checkForBadValue');
-
-console.log(checkForBadValue);
